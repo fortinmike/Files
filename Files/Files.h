868376b5112c79f95539029206d4e488026e79e6
Michaël Fortin <fortinmike@irradiated.net>
2016-09-18 14:37:09 -0400

    Cleanup umbrella header

--- Files/Files.h
+++ Files/Files.h
@@ -6,14 +6,6 @@
 //  Copyright © 2016 irradiated.net. All rights reserved.
 //
 
-//! Project version number for Files.
-FOUNDATION_EXPORT double FilesVersionNumber;
-
-//! Project version string for Files.
-FOUNDATION_EXPORT const unsigned char FilesVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Files/PublicHeader.h>
-
 #import <Files/Directory.h>
 #import <Files/File.h>
 
